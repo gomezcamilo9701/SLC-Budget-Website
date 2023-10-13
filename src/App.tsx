6e7dd690c56712474059b9f07cee40e2ddf39df6
gomezcamilo9701 <gomezcamilo9701@gmail.com>
2023-10-13 10:31:24 -0500

    Adding API route and user attributes modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//import Home from './components/home/Home.tsx';
+//import LoginForm from './components/login/Login';
 import RegisterForm from './components/register/Register';
 
 
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RegisterForm />} /> {/* Este ruta es momentanea mientras se hace el home
-        <Route path="/home" element={<Home />} /> */}
+        {/* <Route path="/" element={<LoginForm />} /> 
+        <Route path="/LoginForm" element={<LoginForm />} />  */}
         <Route path="/register" element={<RegisterForm />} />
       </Routes>
     </Router>
