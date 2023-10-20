1860660f6318ce627929e2fb954679bc241a519d
Leonardo Osorio <cristian.gordillo@correounivalle.edu.co>
2023-10-19 21:27:38 -0500

    Home view update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginForm from './components/login/Login';
-
+import Home from './components/home/Home';
 import RegisterForm from './components/register/Register';
 import './components/register/Register.css';
+import './components/home/Home.css';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RegisterForm />} /> 
+        <Route path="/" element={<LoginForm />} /> 
         <Route path="/login" element={<LoginForm />} /> 
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </Router>
   );
