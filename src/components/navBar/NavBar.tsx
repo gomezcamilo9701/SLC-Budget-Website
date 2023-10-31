959cbb9b432c095196d14e03208514ce2042d5d1
Leonardo Osorio <cristian.gordillo@correounivalle.edu.co>
2023-10-31 17:26:31 -0500

    feat(navBarDrawerUpdate)

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,21 +1,139 @@
-import {
-  AppBar,
-  Box,
-  Container,
-  Toolbar,
-} from "@mui/material";
-import React from "react";
+import * as React from 'react';
+import {  Box,  Toolbar} from "@mui/material";
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import { theme } from '../materialUI-common';
+import { useStyles } from '../home/HomeStyles';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems, secondaryListItems } from '../navBar/listItems';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+
+const drawerWidth: number = 240;
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      backgroundColor: theme.palette.primary.dark,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
 
 export const NavBar: React.FC = () => {
-
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
+    <Box sx={{ }}>
+      <AppBar position="absolute" sx={{ color: theme.palette.primary.light, backgroundColor: theme.palette.secondary.dark }} open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Página de Inicio
+            </Typography>
+            <Stack direction="row" spacing={3}>
+              <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <Button variant='outlined'>
+                Cerrar sesión
+              </Button>
+            </Stack>
           </Toolbar>
-        </Container>
-      </AppBar>
+        </AppBar>
+
+        <Drawer variant="permanent" open={open}>
+          <Box sx={useStyles.boxDrawer}>
+            <Toolbar
+              sx={useStyles.toolbarDrawer}
+            >
+              <img src="src/assets/logo-slc.svg" alt="SLC Logo" style={useStyles.logo} />
+              <IconButton onClick={toggleDrawer} sx={{ color: "#FFF" }}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              {mainListItems}
+              <Divider sx={{ my: 1 }} />
+              {secondaryListItems}
+            </List>
+          </Box>
+        </Drawer>
     </Box>
   );
 };
