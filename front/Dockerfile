60ae4b2bc8f19c655a01d20a1e092e5808ba4466
gomezcamilo9701 <gomezcamilo9701@gmail.com>
2023-11-14 14:56:27 -0500

    test deploy

--- front/Dockerfile
+++ front/Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 5173
+EXPOSE 8000
 CMD ["npm", "run", "dev"]
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'http://ec2-54-221-52-14.compute-1.amazonaws.com:8080',
+        ws: true,
+      },
+    },
+    host: true,
+    port: 8000,
+  },
+});
