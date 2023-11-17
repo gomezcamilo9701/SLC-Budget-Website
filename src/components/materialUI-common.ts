ab6d3ef71c6b5e97f05a035ca6e071fb2a93404e
Leonardo Osorio <cristian.gordillo@correounivalle.edu.co>
2023-11-17 13:20:50 -0500

    feat: drawer view update

--- src/components/materialUI-common.ts
+++ src/components/materialUI-common.ts
@@ -1,6 +1,25 @@
-import { Box, styled} from '@mui/material';
+import { Box, ListItemButton, styled} from '@mui/material';
 import { TableCell, TableRow, createTheme } from '@mui/material';
 import { Theme } from '@mui/material/styles';
+
+// Define your ListItemButton variant
+export const MyListItemButton = styled(ListItemButton)(({ theme }) => ({
+  mt: 4,
+  mb: 4,
+  color: "white",
+  backgroundColor: "#000",
+  border: '3px solid',
+  borderImage: 'linear-gradient(to right, #77EBEB, #9A40E0)',
+  borderImageSlice: 1,
+  padding: '10px 20px 10px 20px',
+  borderRadius: theme.shape.borderRadius,
+  boxShadow: "0px 4px 61px 0px rgba(77, 71, 195, 0.60)",
+  '&:hover': {
+    backgroundColor: "#211f42"
+  },
+  marginTop: theme.spacing(4),
+  marginBottom: theme.spacing(4),
+}));
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
   '&.MuiTableCell-head': {
@@ -24,6 +43,14 @@ export const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 
 const getStyles = (theme: Theme) => ({
+  MuiListItemButton: {
+    styleOverrides: {
+      root: {
+        // No need to add anything here
+      },
+    },
+  },
+
   MuiTableCell: {
     styleOverrides: {
       root: {
